ee37188c0e985f3a61ef5f3d8e5214ee9735f5d3
digital-animal <zahidulislamssf@gmail.com>
2023-01-04 11:18:45 +0600

    postgres integrated

--- src/main/java/com/zahid/Application.java
+++ src/main/java/com/zahid/Application.java
@@ -2,9 +2,11 @@ package com.zahid;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 
 @SpringBootApplication
+// @SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
